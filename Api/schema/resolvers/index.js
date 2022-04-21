e163b35feb23423fec28ba5dd7d9161585c63cb0
natnael-OQP <makiya112545@gmail.com>
2022-04-21 23:31:23 +0300

    import AuthUser

--- Api/schema/resolvers/index.js
+++ Api/schema/resolvers/index.js
@@ -1,7 +1,9 @@
 const { Query } = require('./Query')
 const { Mutation } = require('./Mutation')
+const { AuthUser } = require('./AuthUser')
 
 module.exports = {
     Query,
     Mutation,
+    AuthUser,
 }
