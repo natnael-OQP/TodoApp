376c68c9e6936120eff0bb7000a4f7565059d035
natnael-OQP <makiya112545@gmail.com>
2022-04-22 11:27:49 +0300

    User resolver

--- Api/schema/resolvers/index.js
+++ Api/schema/resolvers/index.js
@@ -1,9 +1,11 @@
 const { Query } = require('./Query')
 const { Mutation } = require('./Mutation')
 const { AuthUser } = require('./AuthUser')
+const { User } = require('./User')
 
 module.exports = {
     Query,
     Mutation,
     AuthUser,
+    User,
 }
