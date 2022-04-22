d7f417f67956f244bcae475c9796742ffa1dc827
natnael-OQP <makiya112545@gmail.com>
2022-04-22 11:28:53 +0300

    signUp user logic completed

--- Api/schema/resolvers/Mutation.js
+++ Api/schema/resolvers/Mutation.js
@@ -1,3 +1,20 @@
+const bcrypt = require('bcryptjs')
+
 exports.Mutation = {
-    signUp: (parent, args, context) => {},
+    signUp: async (_, { input }, { db }) => {
+        const { User } = db
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(input.password, salt)
+
+        const newUser = {
+            ...input,
+            password: hashPassword,
+        }
+        const user = await User.create(newUser)
+        return { token: hashPassword, user }
+    },
+    signIn: (parent, { input }, { db }) => {
+        console.log(args)
+        return []
+    },
 }
