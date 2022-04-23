662b2dccb29e46b2d6dbda05d80ee72e114c4a3c
natnael-OQP <makiya112545@gmail.com>
2022-04-24 01:15:00 +0300

    myTaskList query completed

--- Api/schema/resolvers/Query.js
+++ Api/schema/resolvers/Query.js
@@ -1,3 +1,10 @@
 exports.Query = {
-    myTaskList: () => [],
+    myTaskList: async (_, args, { db: { TaskList }, authUser }) => {
+        if (!authUser)
+            throw new Error("please login first your't Authenticated ")
+
+        return await TaskList.find({
+            userIds: { $in: [authUser._id] },
+        })
+    },
 }
