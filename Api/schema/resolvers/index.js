e2f3415dbbe099816429af12c9c4529242687399
natnael-OQP <makiya112545@gmail.com>
2022-04-26 12:38:13 +0300

    todo resolver

--- Api/schema/resolvers/index.js
+++ Api/schema/resolvers/index.js
@@ -2,10 +2,12 @@ const { Query } = require('./Query')
 const { Mutation } = require('./Mutation')
 const { AuthUser } = require('./AuthUser')
 const { TaskList } = require('./TaskList')
+const { Todo } = require('./Todo')
 
 module.exports = {
     Query,
     Mutation,
     AuthUser,
     TaskList,
+    Todo,
 }
