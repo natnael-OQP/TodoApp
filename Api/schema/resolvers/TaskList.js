ccab5db4821ade756368a8f515a90db73332f9f2
natnael-OQP <makiya112545@gmail.com>
2022-04-26 12:39:13 +0300

    get todos

--- Api/schema/resolvers/TaskList.js
+++ Api/schema/resolvers/TaskList.js
@@ -2,4 +2,6 @@ exports.TaskList = {
     users: async ({ userIds }, _, { db: { User }, authUser }) =>
         await Promise.all(userIds.map((userId) => User.findById(userId))),
     progress: (parent, args, { db, authUser }) => 0.2,
+    todos: async ({ id, _id }, _, { db: { Todo }, authUser }) =>
+        await Todo.find({ taskListId: id || _id }),
 }
