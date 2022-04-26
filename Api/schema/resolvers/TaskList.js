852f980989e91fea44e17730cbe89f3e4b837644
natnael-OQP <makiya112545@gmail.com>
2022-04-26 20:42:01 +0300

    progress completed

--- Api/schema/resolvers/TaskList.js
+++ Api/schema/resolvers/TaskList.js
@@ -1,7 +1,14 @@
 exports.TaskList = {
     users: async ({ userIds }, _, { db: { User }, authUser }) =>
         await Promise.all(userIds.map((userId) => User.findById(userId))),
-    progress: (parent, args, { db, authUser }) => 0.2,
+    progress: async ({ id, _id }, _, { db: { Todo }, authUser }) => {
+        const totalTodos = await Todo.find({ taskListId: id || _id })
+        const completed = totalTodos.filter((todo) => todo.isCompleted)
+        if (totalTodos.length === 0) {
+            return 0.0
+        }
+        return (completed.length / totalTodos.length) * 100
+    },
     todos: async ({ id, _id }, _, { db: { Todo }, authUser }) =>
         await Todo.find({ taskListId: id || _id }),
 }
