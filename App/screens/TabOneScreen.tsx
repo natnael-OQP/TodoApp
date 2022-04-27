3f2f36731f51c8920962639c87ef83ddfe3826d5
natnael-OQP <makiya112545@gmail.com>
2022-04-28 01:28:46 +0300

    todoItem component created

--- App/screens/TabOneScreen.tsx
+++ App/screens/TabOneScreen.tsx
@@ -1,32 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, TextInput } from 'react-native'
+import { Text, View } from '../components/Themed'
+import { RootTabScreenProps } from '../types'
+import TodoItem from '../components/TodoItem'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+export default function TabOneScreen({
+    navigation,
+}: RootTabScreenProps<'TabOne'>) {
+    const [value, setValue] = useState<Boolean>(false)
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Home</Text>
+            <TodoItem isChecked={value} onPress={() => setValue(!value)} />
+            <TodoItem isChecked={value} onPress={() => setValue(!value)} />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+})
