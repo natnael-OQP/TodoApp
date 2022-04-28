020d076562e6533c9798e7a3799a0a1c9bbad789
natnael-OQP <makiya112545@gmail.com>
2022-04-29 00:43:35 +0300

    todo item created

--- App/components/TodoItem.tsx
+++ App/components/TodoItem.tsx
@@ -1,32 +1,56 @@
+import { useEffect, useRef, useState } from 'react'
 import { StyleSheet, TextInput, View } from 'react-native'
 
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import CheckBox from './CheckBox'
 
 interface Props {
-    isChecked: Boolean
-    onPress: () => void
+    todo: {
+        id: number
+        content: string
+        isCompleted: boolean
+    }
+    onSubmit: () => void
 }
 
-const TodoItem = (props: Props) => {
+const TodoItem = ({ todo, onSubmit }: Props) => {
+    const { content, id, isCompleted } = todo
+    const [input, setInput] = useState<string>(content)
+    const [isChecked, setIsChecked] = useState(isCompleted)
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        if (!todo) return
+        setIsChecked(isCompleted)
+        setInput(content)
+    }, [todo])
+
+    useEffect(() => {
+        if (inputRef?.current) inputRef?.current.focus()
+    }, [inputRef])
+
+    const onKeyPress = ({ nativeEvent: { key: keyValue } }) => {
+        if (keyValue === 'Backspace' && input === '') {
+            console.warn('delete item')
+        }
+    }
+
     return (
         <View style={styles.wrapper}>
-            {props.isChecked ? (
-                <MaterialCommunityIcons
-                    onPress={props.onPress}
-                    name="checkbox-marked-outline"
-                    size={27}
-                    color="white"
-                />
-            ) : (
-                <MaterialCommunityIcons
-                    onPress={props.onPress}
-                    name="checkbox-blank-outline"
-                    size={27}
-                    color="white"
-                />
-            )}
-
-            <TextInput style={styles.input} placeholder="todo" multiline />
+            <CheckBox
+                isChecked={isChecked}
+                onPress={() => setIsChecked(!isChecked)}
+            />
+            <TextInput
+                ref={inputRef}
+                value={input}
+                onChangeText={setInput}
+                style={styles.input}
+                placeholder="todo"
+                multiline
+                onSubmitEditing={onSubmit}
+                blurOnSubmit
+                onKeyPress={onKeyPress}
+            />
         </View>
     )
 }
