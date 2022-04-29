f3413fb8e2bf4c4c055a121b89070a1f5d62131c
natnael-OQP <makiya112545@gmail.com>
2022-04-29 23:26:40 +0300

    style tasklist

--- App/screens/TabTwoScreen.tsx
+++ App/screens/TabTwoScreen.tsx
@@ -1,31 +1,56 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from '../components/Themed'
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.wrapper}>
+                <View style={styles.icons}>
+                    <MaterialCommunityIcons
+                        name="file-outline"
+                        color="grey"
+                        size={30}
+                    />
+                </View>
+                <Text style={styles.title} numberOfLines={1}>
+                    MERN Stack project basse
+                </Text>
+                <Text style={styles.time}>2d</Text>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    wrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '100%',
+        padding: 10,
+    },
+    icons: {
+        width: 42,
+        height: 42,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 6,
+        backgroundColor: '#404040',
+    },
+    title: {
+        fontSize: 23,
+        fontWeight: '500',
+        flex: 1,
+        marginHorizontal: 10,
+    },
+    time: {
+        fontSize: 16,
+        color: 'darkgrey',
+    },
+})
