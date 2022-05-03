59cde890828f42554c71707d65a6a9465ca77228
natnael-OQP <makiya112545@gmail.com>
2022-05-03 22:16:54 +0300

    wrapp with apolloProvider

--- App/App.tsx
+++ App/App.tsx
@@ -1,22 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
+
+import { ApolloProvider } from '@apollo/client'
+import { client } from './Apollo'
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+    const isLoadingComplete = useCachedResources()
+    const colorScheme = useColorScheme()
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+    if (!isLoadingComplete) {
+        return null
+    } else {
+        return (
+            <SafeAreaProvider>
+                <ApolloProvider client={client}>
+                    <Navigation colorScheme={colorScheme} />
+                    <StatusBar />
+                </ApolloProvider>
+            </SafeAreaProvider>
+        )
+    }
 }
