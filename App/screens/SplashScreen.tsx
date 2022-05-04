583ca69b4f94a3be4c9f067e6af4fd779ede14ae
natnael-OQP <makiya112545@gmail.com>
2022-05-04 21:42:17 +0300

    get token fromlocal storage

--- App/screens/SplashScreen.tsx
+++ App/screens/SplashScreen.tsx
@@ -1,9 +1,24 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 const SplashScreen = ({ navigation }: any) => {
-    const user = 0
+    const [user, setUser] = useState(false)
+
+    const authUser = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('token')
+            setUser(!!jsonValue)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    console.log(user)
+
     useEffect(() => {
+        authUser()
         if (user) {
             navigation.navigate('Home')
         } else {
